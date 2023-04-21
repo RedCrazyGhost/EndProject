eb5adbf7a9283c5678359a535f6cdb266b40b177
Ghost <zhan940842903@163.com>
2023-04-21 22:02:23 +0800

    添加工具

---
+++ core/util.go
@@ -0,0 +1,93 @@
+/**
+ @author: RedCrazyGhost
+ @date: 2023/4/10
+
+**/
+
+package core
+
+import (
+	"github.com/gin-gonic/gin"
+	"strings"
+)
+
+type Response struct {
+	C *gin.Context
+}
+
+func (g *Response) Send(code int, message string, data interface{}) {
+	g.C.JSON(200, gin.H{
+		"code": code,
+		"data": data,
+		"msg":  message,
+	})
+}
+
+func (g *Response) SendMsg(code int, message string) {
+	g.C.JSON(200, gin.H{
+		"code": code,
+		"data": "",
+		"msg":  message,
+	})
+}
+
+func (g *Response) SendData(code int, data interface{}) {
+	g.C.JSON(200, gin.H{
+		"code": code,
+		"data": data,
+		"msg":  "",
+	})
+}
+
+func (g *Response) SuccessMsg(message string) {
+	g.C.JSON(200, gin.H{
+		"code": 1,
+		"data": "",
+		"msg":  message,
+	})
+}
+
+func (g *Response) ErrorMsg(message string) {
+	g.C.JSON(200, gin.H{
+		"code": 0,
+		"data": "",
+		"msg":  message,
+	})
+}
+
+func (g *Response) SuccessData(data interface{}) {
+	g.C.JSON(200, gin.H{
+		"code": 200,
+		"data": data,
+		"msg":  "成功",
+	})
+}
+
+func (g *Response) ErrorData(data interface{}) {
+	g.C.JSON(200, gin.H{
+		"code": 0,
+		"data": data,
+		"msg":  "",
+	})
+}
+
+func (g *Response) Error(msg ...string) {
+	if len(msg) == 0 {
+		msg = append(msg, "系统错误")
+	}
+	g.C.JSON(500, gin.H{
+		"code": 500,
+		"data": "",
+		"msg":  strings.Join(msg, ""),
+	})
+}
+
+// SetValidateError 验证错误
+func (g *Response) SetValidateError(err error) *gin.Error {
+	return g.C.Error(err).SetType(gin.ErrorTypeBind)
+}
+
+// SetOtherError 数据库异常
+func (g *Response) SetOtherError(err error) *gin.Error {
+	return g.C.Error(err).SetType(gin.ErrorTypePublic)
+}
